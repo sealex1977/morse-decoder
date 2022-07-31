4643e4a1787970a077dd08bb6686d9ab16bb81a7
Oleksandr Serebriakov <sealex1977@gmail.com>
2022-07-31 14:05:48 +0300

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -38,7 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+        // write your solution here
+        let arrMorse = [];
+        let strMorse = '';
+
+        for (let i = 0; i < expr.length; i += 10) {
+        let str = '';
+
+            for (j = 0; j < 10; j += 2) {
+        if (expr[i + j] == '*') {
+            str += ' ';
+            break;
+        } else if (expr[i + j] + expr[i + j + 1] == '10') {
+            str += '.';
+        } else if (expr[i + j] + expr[i + j + 1] == '11') {
+            str += '-';
+        }
+            }
+        arrMorse.push(str);
+        }
+
+        for (let key in arrMorse) {
+        if (arrMorse[key] == ' ') {
+        strMorse += ' ';
+        } else {
+        strMorse += MORSE_TABLE[arrMorse[key]];
+        } 
+    }
+
+    return strMorse;
 }
 
 module.exports = {
